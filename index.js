23198a3a41ff9a24a1cc1de1aff7b14ab7c8de90
Alexander <support@boosting.store>
2022-12-12 09:58:07 +0300

    now you can add cat

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const insideBodyContent = document.querySelector('body > div.d-grid.gap-1 > div 
 const modalCloseButton = document.querySelector('#staticBackdrop > div > div > div.modal-footer > button');
 // username в хедаре
 const headerUserName = document.querySelector('body > div.d-grid.gap-1 > header > div > div > form > div');
+// Добавить котика
+const addCatButton = document.querySelector('#addСatForm > div > div > form > div.modal-footer > button.btn.btn-primary');
+// fetch запрос
+let myFetch = {};
 // Модальное окно при загрузке сайта
 function ready() {
   setTimeout(() => {
@@ -29,10 +33,8 @@ document.querySelector('body > div.d-grid.gap-1 > header > div > div > form > di
     if (insideBodyContent.childNodes.length) {
       insideBodyContent.textContent = '';
     }
-    console.log(document.getElementsByTagName('input')[0].value);
     myLogIn = document.getElementsByTagName('input')[0].value;
     modalCloseButton.click();
-    // document.querySelector('body > div.d-grid.gap-1 > header > div > div > form > div').classList.toggle('visually-hidden');
     contentLoad();
   }
 });
@@ -41,10 +43,8 @@ document.querySelector('body > div.d-grid.gap-1 > header > div > div > form > di
   if (insideBodyContent.childNodes.length) {
     insideBodyContent.textContent = '';
   }
-  console.log(document.getElementsByTagName('input')[0].value);
   myLogIn = document.getElementsByTagName('input')[0].value;
   modalCloseButton.click();
-  // document.querySelector('body > div.d-grid.gap-1 > header > div > div > form > div').classList.toggle('visually-hidden');
   contentLoad();
 });
 
@@ -57,7 +57,6 @@ document.querySelector('#staticBackdrop > div > div > div.modal-body > form > di
     if (insideBodyContent.childNodes.length) {
       insideBodyContent.textContent = '';
     }
-    console.log(document.getElementsByTagName('input')[1].value);
     myLogIn = document.getElementsByTagName('input')[1].value;
     modalCloseButton.click();
     headerUserName.classList.toggle('visually-hidden');
@@ -69,13 +68,60 @@ document.querySelector('#staticBackdrop > div > div > div.modal-body > form > di
   if (insideBodyContent.childNodes.length) {
     insideBodyContent.textContent = '';
   }
-  console.log(document.getElementsByTagName('input')[1].value);
   myLogIn = document.getElementsByTagName('input')[1].value;
   modalCloseButton.click();
   headerUserName.classList.toggle('visually-hidden');
   contentLoad();
 });
 // конец
+
+// Обработчик добавить котика https://doka.guide/js/deal-with-forms/
+function serializeForm(formNode) {
+  const { elements } = formNode;
+
+  const data = Array.from(elements)
+    .map((element) => {
+      const { name, type } = element;
+      const value = type === 'checkbox' ? element.checked : element.value;
+
+      return { name, value };
+    })
+    .filter((item) => !!item.name);
+
+  const myCatObj = {
+    id: data[0].value,
+    name: data[1].value,
+    image: data[2].value,
+    age: data[3].value,
+    rate: data[4].value,
+    favorite: data[6].value,
+    description: data[5].value,
+  };
+  myFetch = myCatObj;
+}
+
+async function sendData(data) {
+  console.log(data);
+  return await fetch(`https://cats.petiteweb.dev/api/single/${myLogIn}/add/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const data = serializeForm(event.target);
+
+  const response = await sendData(myFetch);
+}
+
+const addCatForm = document.querySelector('#addCat');
+addCatForm.addEventListener('submit', handleFormSubmit);
+//
 
 // Получаем котиков
 function contentLoad() {
@@ -110,3 +156,34 @@ const getCatHTMLv2 = (cat) => `<div class="col">
 // конец
 // ZholobovSS
 // Yanlex
+// {
+//   "id": 1,
+//   "name": "Имя котика",
+//   "image": "Ссылка на изображение",
+//   "age": 0,
+//   "rate": 0,
+//   "favorite": false,
+//   "description": ""
+// }
+// data[0].value
+// 0
+// :
+// {name: 'cat-id', value: ''}
+// 1
+// :
+// {name: 'cat-name', value: ''}
+// 2
+// :
+// {name: 'cat-image', value: ''}
+// 3
+// :
+// {name: 'cat-age', value: ''}
+// 4
+// :
+// {name: 'floatingSelect', value: 'Оценить котика'}
+// 5
+// :
+// {name: 'cat-description', value: ''}
+// 6
+// :
+// {name: 'cat-fav', value: false}
